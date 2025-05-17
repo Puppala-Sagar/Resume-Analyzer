be063faff6db2fb350f939c15922f3744b66ed91
puppala-sagar <sagarpuppala123@gmail.com>
2025-05-17 22:17:49 +0530

    Add gunicorn to requirements

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Remove this line since we're already in backend/: 
-# cd backend  # <- Comment out or delete this line
+
 pip install -r requirements.txt
 python -m spacy download en_core_web_md
 python -m nltk.downloader stopwords
