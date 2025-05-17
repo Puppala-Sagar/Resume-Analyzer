97fd8fbb92fee7026b9af9f00a2ded3151978744
Puppala-Sagar <138113266+Puppala-Sagar@users.noreply.github.com>
2025-05-17 21:45:09 +0530

    Update build.sh

--- Backend/build.sh
+++ Backend/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd backend  # Navigate to the backend folder first
 pip install -r requirements.txt
 python -m spacy download en_core_web_md
 python -m nltk.downloader stopwords
